f75e9a46a26ec1cd531c5bbdafa4a5556f9253ec
Abel <abel.lavieri@gmail.com>
2020-09-29 15:34:28 -0400

    Work in some requested changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
@@ -19,9 +18,8 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
     if @user.update(user_params)
-      redirect_to new_user_path, notice: 'User updated'
+      redirect_to edit_user_path, notice: 'User updated'
     else
       render :edit
     end
