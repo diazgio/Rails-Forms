bf62694af26f781bf616afac68310d4c3c9df751
Abel <abel.lavieri@gmail.com>
2020-09-29 13:30:32 -0400

    Change the default page to the new users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create edit update]
+  root to: 'users#new'
 end
